e968e4fffe09e42fbbcbbcf25133c0cbd42d5ab0
Adam Hariz <adamhariz191@gmail.com>
2026-06-12 09:04:52 +0800

    remove

--- js/data.js
+++ js/data.js
@@ -100,7 +100,7 @@ const CHAPTERS = [
   {
     id: 4, dictKey: "ch4", subtitle: "Emergency", icon: "🆘", color: "#F44336",
     levels: [
-      { id: "4-1", dictKey: "lvl_first_resp", signs: ["Help", "Medicine", "Dizzy"], xp: 500 },
+      { id: "4-1", dictKey: "lvl_first_resp", signs: ["Help","Dizzy"], xp: 500 },
       { id: "4-2", dictKey: "lvl_imm_alert", signs: ["Danger", "Stop"], xp: 500 },
       { id: "4-3", dictKey: "lvl_med_haz", signs: ["Doctor", "Hospital", "Nurse"], xp: 500 },
       { id: "4-4", dictKey: "lvl_sec_safe", signs: ["Police", "Thief", "Fire"], xp: 500 },
